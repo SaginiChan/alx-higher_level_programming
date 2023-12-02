f981066f4b1ad49bc6d8da1534654287ebc55f78
SaginiChan <boazsagini62@gmail.com>
2023-12-02 10:29:26 -0800

    0x00

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+/**
+ * check_cycle - checks for a cycle
+ * @list: the linked list to check
+ * Return: 1 for a cycle, 0 if not
+ *
+ */
+int check_cycle(listint_t *list)
+{
+	listint_t *slow = list;
+	listint_t *fast = list;
+	
+	if (!list)
+		return (0);
+	while (slow && fast && fast->next)
+	{
+		slow = slow->next;
+		fast = fast->next->next;
+		if (slow == fast)
+			return (1);
+	}
+	return (0);
+}
